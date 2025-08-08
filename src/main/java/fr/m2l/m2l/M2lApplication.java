9c5d6d52cebc91b130e59329b046474135031f5d
Mewweb <mewen.rai09@gmail.com>
2025-08-08 21:51:47 +0200

    Ajout du projet Spring

---
+++ src/main/java/fr/m2l/m2l/M2lApplication.java
@@ -0,0 +1,13 @@
+package fr.m2l.m2l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2lApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2lApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/m2l/m2l/M2lApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.m2l.m2l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2lApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
